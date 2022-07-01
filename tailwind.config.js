0074974c93c807bf038a264f25dbe9a28cb978ac
Kehinde Hussein, Fasunle <kfasunle@gmail.com>
2022-07-01 17:30:35 +0100

    feat: Tailwind cointainer theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,16 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    container:{
+      padding: {
+        DEFAULT: '1rem',
+        sm: "2rem",
+        md: "3rem",
+        lg: "4rem",
+        xl: "5rem",
+        '2xl': "6rem",
+      }
+    },
   },
   plugins: [
     require('@tailwindcss/typography'),
