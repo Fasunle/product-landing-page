7b47ce080cbcb43db2de150112928cf01933b888
Kehinde Hussein, Fasunle <kfasunle@gmail.com>
2022-07-01 14:37:42 +0100

    fix: make line responsive

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,14 +1,16 @@
 <template>
     <header>
-        <nav class="container flex place-content-between py-5 nav">
+        <nav class="flex place-content-between py-5 nav">
             <div class="flex place-content-center">
-                <div class="flex justify-center items-center md:hidden">
-                    <img class="h-6 w-6" src="../assets/icon-menu.svg" alt="Menu Icon">
+                <div class="flex justify-center items-center md:hidden w-10 px-2">
+                    <img class="h-5 w-full" src="../assets/icon-menu.svg" alt="Menu Icon">
                 </div>
-                <a href="/" class="ml-8 mr-4">
-                    <h1 class="brand">Sneakers</h1>
-                </a>
-                <ul class="flex md:hidden place-items-center nav-items">
+
+                <span class="font-semibold md:text-3xl sm:text-lg mr-8 inline-block place-content-center my-auto">
+                    <a href="/">Sneakers</a>
+                </span>
+
+                <ul class="flex sx:hidden mx-5 place-items-center nav-items">
                     <li class="mr-4"><a href="#">Collections</a> </li>
                     <li class="mr-4"><a href="#">Men</a></li>
                     <li class="mr-4"><a href="#">Women</a></li>
@@ -17,16 +19,17 @@
                 </ul>
             </div>
 
-            <div class="flex imageContainer">
-                <div class="mr-4 flex justify-center items-center">
-                    <img  src="../assets/cart.png" alt="Cart Icon">
+            <div class="flex user-info">
+                <div class="sm:mr-2 md:mr-4 w-14">
+                    <img class="aspect-1" src="../assets/cart.png" alt="Cart Icon">
                 </div>
-                <div class="mx-4">
-                    <img src="../assets/image-avatar.png" alt="User Avatar">
+                <div class="w-14">
+                    <img class="aspect-1" src="../assets/image-avatar.png" alt="User Avatar">
                 </div>
             </div>
 
         </nav>
+        <div class="h-0.5 w-full bg-gray-300"></div>
     </header>
 </template>
 
@@ -36,27 +39,19 @@
 
 
 
+
+
+
     a{
         cursor: pointer;
     }
-    .brand{
-        font-size: 1.85rem;
-        font-weight: 700;
-    }
-
-    /* Bottom Line */
-    .nav{
-        position: relative;
-    }
-
-    .nav::after{
-        position: absolute;
-        content: "";
-        left: 0;
+    
+    .line{
+        height: 20px;
         width: 100%;
-        border-bottom: 1px solid hsl(219 9% 45% / .75);
-        bottom: 0;
+        background-color: green;
     }
+
 
     .nav-items a{
         position: relative;
@@ -64,7 +59,7 @@
 
     .nav-items a::after{
         border-bottom: 5px solid hsl(26 100% 55% / 0);
-        bottom: -130%;
+        bottom: -140%;
         content: "";
         position: absolute;
         left: 0;
@@ -76,11 +71,7 @@
         border-bottom-color: hsl(26 100% 55% / 1);
     }
     
-    .imageContainer > div{
-        width: 50px;
-    }
-
-    .imageContainer img{
+    .user-info img{
         display: block;
         width: 100%;
         border-radius: 50%;
@@ -90,7 +81,7 @@
         border: 2px solid hsl(26 100% 55% / 0);
     }
 
-    .imageContainer img:hover{
+    .user-info img:hover{
         
         border-color: hsl(26 100% 55% / 1);
     }
@@ -100,14 +91,11 @@
             display: none;
         }
 
-        .container{
-            padding-inline: 1em;
-        }
     }
 
-    @media (min-width: 401px){
+    @media (max-width: 772px){
         .nav-items{
-            display: flex;
+            display: none;
         }
 
     }
