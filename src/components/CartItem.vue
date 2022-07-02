d80875966693132a3c37c2eb8a819fba899800db
Kehinde Hussein, Fasunle <kfasunle@gmail.com>
2022-07-02 11:08:31 +0100

    feat: Cart components

---
+++ src/components/CartItem.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="flex justify-between items-center w-full my-2 gap-4">
+        <div class="w-8 h-8">
+            <img class="rounded-md" :src="url" alt="Product thumbnail">
+        </div>
+
+        <div class="w-5/6 overflow-clip">
+            <h3>{{name}}</h3>
+            <p>
+                ${{price}} x {{quantity}} <span>${{total}}</span>
+            </p>
+        </div>
+
+        <div>
+            <img src="../assets/icon-delete.svg" alt="Delete Product">
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "CartItem",
+    props:{
+        id: String,
+        name: String,
+        price: Number,
+        quantity: Number,
+        total: Number,
+        url: String
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/CartView.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="bg-gray-50 rounded-md shadow-2xl absolute p-cart-mobile sm:p-cart-desktop sm:fixed ">
+
+    <h1 class="p-2 font-semibold text-black text-lg">Cart</h1>
+
+    <span class="bg-gray-300 h-0.5 block"></span>
+
+    <div class="grid grid-cols-1 grid-flow-row py-4 h-2/3 sm:h-3/4 overflow-y-auto snap-y scrollbar-hide">
+      <div v-if="cartItems.length == 0" class="flex place-self-center snap-center">Your Cart is empty.</div>
+
+      <div v-else-if="cartItems.length >= 0" v-for="cart in cartItems" class="mx-8 snap-center"
+        :key="cart.id">
+        <CartItem :name="cart.name" :price="60" :quantity="2" :total="120"
+          :url="'https://avatars.githubusercontent.com/u/77301273?v=4'" />
+        <CartItem :name="cart.name" :price="60" :quantity="2" :total="120"
+          :url="'https://avatars.githubusercontent.com/u/77301273?v=4'" />
+        <CartItem :name="cart.name" :price="60" :quantity="2" :total="120"
+          :url="'https://avatars.githubusercontent.com/u/77301273?v=4'" />
+        <CartItem :name="cart.name" :price="60" :quantity="2" :total="120"
+          :url="'https://avatars.githubusercontent.com/u/77301273?v=4'" />
+        <CartItem :name="cart.name" :price="60" :quantity="2" :total="120"
+          :url="'https://avatars.githubusercontent.com/u/77301273?v=4'" />
+        <CartItem :name="cart.name" :price="60" :quantity="2" :total="120"
+          :url="'https://avatars.githubusercontent.com/u/77301273?v=4'" />
+        <CartItem :name="cart.name" :price="60" :quantity="2" :total="120"
+          :url="'https://avatars.githubusercontent.com/u/77301273?v=4'" />
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<script>
+import CartItem from './CartItem.vue'
+export default {
+    name: "CartView",
+    data() {
+        return {
+            cartItems: [{
+                    name: "Sneaker 1",
+                    id: 1
+                }]
+        };
+    },
+    components: { CartItem }
+}
+</script>
+
+<style>
+
+</style>
